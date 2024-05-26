b59eccb9d968cba6ac690c5a63024c1f8d34087a
cristiangrave <125296137+cristiangrave@users.noreply.github.com>
2024-05-25 19:27:33 -0600

    Añadiento el Dockerfile

--- src/components/AddingMobileButton/AddingMobileButton.js
+++ src/components/AddingMobileButton/AddingMobileButton.js
@@ -1,12 +1,13 @@
-import Button from 'react-bootstrap/Button';
-import './AddingMobileButton.scss'
+import Button from "react-bootstrap/Button";
+import "./AddingMobileButton.scss";
 
 function AddingMobileButton() {
   return (
     <>
-      <Button variant="info" className='btn-addgoal'>Add Goal</Button>
+      <Button variant="info" className="btn-addgoal">
+        Add Goal
+      </Button>
     </>
   );
 }
-
 export default AddingMobileButton;
